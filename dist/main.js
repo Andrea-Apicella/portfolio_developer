2644a6bad9ecb1e66a8bb4d41aa6c38f14346373
Andrea-Apicella <Andrea-Apicella@users.noreply.github.com>
2020-10-27 00:27:26 +0100

    started changing hamburger structure

--- dist/main.js
+++ dist/main.js
@@ -1,12 +1,10 @@
 const toggleButton = document.querySelector('#toggle-button');
 const navbarLinks = document.querySelector('.navbar-links');
 const navLinks = document.querySelectorAll('.nav-link');
-
 toggleButton.addEventListener('click', (e) => {
 	e.preventDefault;
 	navbarLinks.classList.toggle('active');
 });
-
 
 // change active item on click: removes active class from items who have it and adds active class to the clicked item.
 navLinks.forEach((link) => {
