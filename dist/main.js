31cf254839ae2ae7875682fce373b60917c554fc
Andrea-Apicella <Andrea-Apicella@users.noreply.github.com>
2020-10-26 23:42:27 +0100

    navbar styling: navlinks underline bar with animation, active class js function

--- dist/main.js
+++ dist/main.js
@@ -1,7 +1,21 @@
-const toggleButton = document.querySelector('#toggle-button-container');
+const toggleButton = document.querySelector('#toggle-button');
 const navbarLinks = document.querySelector('.navbar-links');
+const navLinks = document.querySelectorAll('.nav-link');
 
 toggleButton.addEventListener('click', (e) => {
-  e.preventDefault;
+	e.preventDefault;
 	navbarLinks.classList.toggle('active');
+});
+
+
+// change active item on click: removes active class from items who have it and adds active class to the clicked item.
+navLinks.forEach((link) => {
+	link.addEventListener('click', () => {
+		navLinks.forEach((link) => {
+			if (link.classList.contains('active')) {
+				link.classList.remove('active');
+			}
+		});
+		link.classList.add('active');
+	});
 });
