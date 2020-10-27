5a5d3e77659cc11719d37bbfe26eb822b9e6aa11
Andrea-Apicella <Andrea-Apicella@users.noreply.github.com>
2020-10-27 11:08:32 +0100

    final navbar version and landingpage small fixes

--- dist/main.js
+++ dist/main.js
@@ -4,6 +4,7 @@ const navLinks = document.querySelectorAll('.nav-link');
 toggleButton.addEventListener('click', (e) => {
 	e.preventDefault;
 	navbarLinks.classList.toggle('active');
+	toggleButton.classList.toggle('clicked');
 });
 
 // change active item on click: removes active class from items who have it and adds active class to the clicked item.
