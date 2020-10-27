ff59b68240787545d9f4712b1036080ab1fb9bf8
Andrea-Apicella <Andrea-Apicella@users.noreply.github.com>
2020-10-27 21:44:04 +0100

    dark mode updates

--- dist/main.js
+++ dist/main.js
@@ -1,6 +1,7 @@
 const toggleButton = document.querySelector('#toggle-button');
 const navbarLinks = document.querySelector('.navbar-links');
 const navLinks = document.querySelectorAll('.nav-link');
+const darkModeToggler = document.querySelector('#dark-mode-toggler');
 toggleButton.addEventListener('click', (e) => {
 	e.preventDefault;
 	navbarLinks.classList.toggle('active');
@@ -18,3 +19,9 @@ navLinks.forEach((link) => {
 		link.classList.add('active');
 	});
 });
+
+
+darkModeToggler.addEventListener('click', ()=>{
+	document.body.classList.toggle('dark');
+
+})
