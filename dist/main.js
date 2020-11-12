5b936d75ad1d4a6a15b16b8c548996d1f071ec4e
Andrea <andrea.apicella221@gmail.com>
2020-11-12 22:03:04 +0100

    color palette changes

--- dist/main.js
+++ dist/main.js
@@ -1,3 +1,4 @@
+
 const toggleButton = document.querySelector('#toggle-button');
 const navbarLinks = document.querySelector('.navbar-links');
 const navLinks = document.querySelectorAll('.nav-link');
